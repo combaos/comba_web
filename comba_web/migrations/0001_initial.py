aa3b995cb6750e389dac7f2db60e936fb36bb6d8
Michael Liebler <michel@janguo.de>
2015-10-22 22:09:14 +0200

    remove combauser migration

--- comba_web/migrations/0001_initial.py
+++ comba_web/migrations/0001_initial.py
@@ -10,16 +10,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='CombaUser',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
-                ('user_role', models.CharField(default=b'SU', max_length=2, choices=[(b'admin', b'admin'), (b'serviceuser', b'Serviceuser'), (b'webuser', b'Webuser')])),
-            ],
-            options={
-                'abstract': False,
-            },
-        ),
     ]
